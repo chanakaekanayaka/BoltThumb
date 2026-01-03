cf0b7c514c4794dfa25d433dc3f016020d1f7fd2
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2026-01-03 23:30:27 +0530

    changes: setup routes ✏️

--- client/src/sections/CTASection.tsx
+++ client/src/sections/CTASection.tsx
@@ -1,8 +1,12 @@
 'use client'
 import { motion } from "framer-motion";
 import { ZapIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function CTASection() {
+
+    const navigate = useNavigate()
+
     return (
         <div className="relative px-4 pb-32 mt-20">
             {/* The Main Container */}
@@ -25,7 +29,7 @@ export default function CTASection() {
                         viewport={{ once: true }}
                         transition={{ delay: 0.2 }}
                     >
-                        Ready to <span className="text-rose-500 italic">Bolt</span> your views?
+                        Ready  to <span className="text-rose-500 italic"> <br></br></span> go viral?
                     </motion.h2>
                     <motion.p 
                         className="text-slate-400 text-lg md:text-xl font-medium max-w-md"
@@ -44,13 +48,15 @@ export default function CTASection() {
                     viewport={{ once: true }}
                     transition={{ delay: 0.4 }}
                 >
-                    <button className="group relative flex items-center gap-3 px-10 py-5 bg-rose-600 hover:bg-rose-500 text-white font-black text-xl rounded-full transition-all hover:scale-105 active:scale-95 shadow-[0_0_30px_rgba(225,29,72,0.3)]">
+                    <button className="group relative flex items-center gap-3 px-10 py-5 bg-rose-600 hover:bg-rose-500
+                     text-white font-black text-xl rounded-full transition-all hover:scale-105 active:scale-95 
+                     shadow-[0_0_30px_rgba(225,29,72,0.3)]" onClick={()=>navigate('/generate')}>
                         <ZapIcon className="size-6 fill-white" />
                         Get Started Now
                         <div className="absolute inset-0 rounded-full border-2 border-white/20 animate-ping group-hover:hidden"></div>
                     </button>
                     <p className="text-slate-500 text-xs text-center mt-4 font-medium uppercase tracking-widest">
-                        No Credit Card Required
+                        Generate Free Thumbnail
                     </p>
                 </motion.div>
             </motion.div>
