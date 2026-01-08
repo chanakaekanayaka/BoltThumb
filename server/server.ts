2c450629c6670a3f50fa7a1199cf16eb653e904b
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2026-01-08 13:54:11 +0530

    user model and session middlware completed✏️

--- server/server.ts
+++ server/server.ts
@@ -2,13 +2,39 @@ import express, { Request, Response } from 'express';
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.js';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+
+declare module 'express-session' {
+    interface SessionData{
+        isLoggedIn: boolean;
+        userId :string;
+    }
+}
 
 await connectDB()
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    credentials:true
+}))
+
+app.use(session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: false,
+    cookie : {maxAge: 1000* 60* 60 * 24* 7}, //7day
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI as string,
+        collectionName: 'sessions'
+    })
+}))
+
 app.use(express.json())
+
+
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Server is Live!');
